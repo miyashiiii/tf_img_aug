3735c65132d7a34ce8491c2e8b2dbd1c88f17b1a
Miyashita Yosuke <dev.miyashiiii@gmail.com>
2019-09-24 19:16:32 +0900

    Output as gif animation

---
+++ images_to_gif.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+
+def images_to_gif(imgs, output_path):
+    pil_imgs = [Image.fromarray(img) for img in imgs]
+    pil_imgs[0].save(output_path, save_all=True, append_images=pil_imgs[1:], duration=500, disposal=2)
+
+
+def images_to_gif_with_param(imgs, output_path, param_name, params, before_str="", after_str=""):
+    new_imgs = []
+    if before_str:
+        before_str += " "
+    if after_str:
+        after_str = " " + after_str
+    for img, param in zip(imgs, params):
+        h, w = img.shape[:2]
+        new_h = h + 15
+        new_img = np.full((new_h, w, 3), 255, dtype=np.uint8)
+        new_img[:h, :] = img
+        cv2.putText(new_img, f"{before_str}{param_name}: {param}{after_str}", (5, new_h - 5), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+        new_imgs.append(new_img)
+    images_to_gif(new_imgs, output_path)
+
+
+def main():
+    file_path_1 = "result_00.jpg"
+    file_path_2 = "result_01.jpg"
+    file_path_3 = "result_02.jpg"
+    file_path_4 = "result_03.jpg"
+    file_path_5 = "result_04.jpg"
+    file_paths = [file_path_1, file_path_2, file_path_3, file_path_4, file_path_5]
+    imgs = [cv2.imread(path) for path in file_paths]
+    output_path = "result.gif"
+    param_name = "num"
+    params = [0, 1, 2, 3, 4]
+    images_to_gif_with_param(imgs, output_path, param_name, params)
+
+
+if __name__ == "__main__":
+    main()
--- tf_img_aug.py
+++ tf_img_aug.py
@@ -1,39 +1,52 @@
 from pathlib import Path
 
-import cv2
 import tensorflow as tf
 
+from images_to_gif import images_to_gif_with_param
 
-def tf_img_aug(img_path, delta):
+
+def adjust_gamma_fixed_gamma(image, gain):
+    return tf.image.adjust_gamma(
+        image,
+        gamma=1,
+        gain=gain
+    )
+
+
+def adjust_gamma_fixed_gain(image, gamma):
+    return tf.image.adjust_gamma(
+        image,
+        gamma=gamma,
+        gain=1
+    )
+
+
+def img_aug_session(img_path, fn, param):
     image_r = tf.io.read_file(img_path)
     image = tf.image.decode_image(image_r, channels=3)
-    image = tf.image.adjust_gamma(
-        image,
-        gain=delta
-    )
-    # image = tf.image.encode_png(image)
 
+    image = fn(image, param)
     return image
+
+
+def img_aug(input_path, output_path, fn, params, before_str="", after_str=""):
+    imgs = []
+    for param in params:
+        with tf.compat.v1.Session() as sess:
+            img = sess.run(img_aug_session(input_path, fn, param))
+
+        imgs.append(img)
+
+    images_to_gif_with_param(imgs, output_path, "gamma", params, before_str, after_str)
 
 
 def main():
     input_dir = Path("input")
     output_dir = Path("output")
-
-    img_name = "000001.jpg"
-    input_path = str(input_dir / img_name)
-
-    for i in range(10):
-        output_path = str(output_dir / f"{i:02}_{img_name}")
-
-        delta = i * 0.2
-        img_aug = tf_img_aug(input_path, delta)
-
-        with tf.compat.v1.Session() as sess:
-            img_rgb = sess.run(img_aug)
-
-        img = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(output_path, img)
+    input_path = str(input_dir / "degu.jpg")
+    params = [round(i * 0.4, 1) for i in range(10)]
+    img_aug(input_path, str(output_dir / "gamma_gamma.gif"), adjust_gamma_fixed_gain, params, after_str="gain: 1")
+    img_aug(input_path, str(output_dir / "gamma_gain.gif"), adjust_gamma_fixed_gamma, params, before_str="gamma: 1")
 
 
 if __name__ == "__main__":
