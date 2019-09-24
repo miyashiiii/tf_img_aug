abe63f24877a50c50732936bf4ab31956240a835
Miyashita Yosuke <dev.miyashiiii@gmail.com>
2019-09-24 21:17:50 +0900

    Rewrite tf_img_aug.main() as usage

--- tf_img_aug.py
+++ tf_img_aug.py
@@ -5,22 +5,6 @@ import tensorflow as tf
 from images_to_gif import images_to_gif_with_param
 
 
-def adjust_gamma_fixed_gamma(image, gain):
-    return tf.image.adjust_gamma(
-        image,
-        gamma=1,
-        gain=gain
-    )
-
-
-def adjust_gamma_fixed_gain(image, gamma):
-    return tf.image.adjust_gamma(
-        image,
-        gamma=gamma,
-        gain=1
-    )
-
-
 def img_aug_session(img_path, fn, param):
     image_r = tf.io.read_file(img_path)
     image = tf.image.decode_image(image_r, channels=3)
@@ -46,9 +30,13 @@ def main():
     input_path = str(input_dir / "degu.jpg")
 
     # adjust_gamma
-    params = [round(i * 0.4, 1) for i in range(10)]
-    img_aug(input_path, "gamma_gamma.gif", adjust_gamma_fixed_gain, params, "gamma", after_str="gain: 1")
-    img_aug(input_path, "gamma_gain.gif", adjust_gamma_fixed_gamma, params, "gain", before_str="gamma: 1")
+    img_aug(
+        input_path=input_path,
+        output_name="gamma_gain.gif",
+        fn=lambda image, gain: tf.image.adjust_gamma(image, 1, gain),
+        params=[round(i * 0.4, 1) for i in range(10)],
+        param_name="gain",
+        before_str="gamma: 1")
 
 
 if __name__ == "__main__":
